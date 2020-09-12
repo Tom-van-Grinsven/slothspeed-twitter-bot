470b6f25c1578cc4c321c69510aa8fe5bbc7b9d1
Tom van Grinsven <tomvangrinsven92@gmail.com>
2020-09-12 18:36:16 +0200

    cleanup

--- slothspeed.js
+++ slothspeed.js
@@ -1,19 +1,12 @@
 const twitterClient = require('./clients/twitterclient');
 const speedTestClient = require('./clients/speedtestclient');
 
-let upload = 0;
-let download = 0;
-
 (async function(){
     try {
-        console.log("Hier");
-
         let response =  await speedTestClient.getSpeed();
-
         twitterClient.tweet(response);
-
     } catch (e){
-        console.log(e)
+        console.error(e);
     }
 })();
 
